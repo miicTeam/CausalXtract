7fbe1951443471069311d9bf9fdb9d427e3c6084
tizianatocci <128061109+tizianatocci@users.noreply.github.com>
2023-09-04 10:08:23 +0200

    Delete CellHunterPlus directory

--- CellHunterPlus/Segmentation and Tracking/main_example.m
+++
@@ -1,8 +0,0 @@
-% file of example to apply tracking to a crop
-clear; close all; clc;
-info=imfinfo('E:\CASUAL_X_TRACK\MIIC_OLD_DATA\id20161230_pos1_roi6.tif');
-for k = 1 : length(info)
-    video(:,:,k)=im2double(imread(info(k).Filename,'Index',k));
-end
-
-[p_sp2,q_sp2,data_sp,data_tu] = my_cell_Hunter_Munkres(video);
--- CellHunterPlus/my_files_names.m
+++
@@ -1,8 +0,0 @@
-%% Code for obtaining all the names of the files (rois) in the folder.
-function [all_names, dates, posits, rois]=my_files_names(path_roi)
-all_files=dir(fullfile(path_roi, "*.mat"));
-all_names = {all_files.name}';
-dates=cellfun(@(x) extractBetween(x,'id','_'), all_names, 'UniformOutput', true);
-posits=cellfun(@(x) extractBetween(x,'pos','_'), all_names, 'UniformOutput', true);
-rois=cellfun(@(x) extractBetween(x,'roi','.mat'), all_names, 'UniformOutput', true);
-end
