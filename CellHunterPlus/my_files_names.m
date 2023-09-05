ff15cac8dedc71f167c8506f720e6c4eafc8b3d7
tizianatocci <128061109+tizianatocci@users.noreply.github.com>
2023-09-05 18:30:53 +0200

    Delete CellHunterPlus directory

--- CellHunterPlus/my_files_names.m
+++
@@ -1,7 +0,0 @@
-% Obtain all the names of the files (rois) in the folder.
-
-function roi_names=my_files_names(path_roi)
-all_files=dir(fullfile(path_roi, "*.mat"));
-file_names = {all_files.name}';
-roi_names=cellfun(@(x) extractBefore(x, '.mat'), file_names, 'UniformOutput', false);
-end
